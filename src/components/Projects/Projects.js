1a77b9e98af252c4e0e9b2f3e8ac640558c90836
Pazil24 <azilafupriscilla@gmail.com>
2024-05-14 15:20:40 +0000

    Added images of projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,12 +2,8 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+import sarah from "../../Assets/Projects/sarahPortfolio.png";
+import rencia from "../../Assets/Projects/RenciaTN.png";
 
 function Projects() {
   return (
@@ -23,10 +19,10 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={sarah}
               isBlog={false}
               title="Sarah's Portfolio"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
+              description="This is a group portfolio project of a fictional photographer called Sarah. She is a potrait photographer that specializes in headshots for actors and models. She is based in London but provides mobile services and also available to travel. She currently needs a portfolio website to enable her showcase her work so as to reach more clients."
               ghLink="https://github.com/MutiatBash/Sarahs-Portfolio/tree/main"
               demoLink="https://sarahs-portfolio-project.vercel.app/index.html"
             />
@@ -34,36 +30,14 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={rencia}
               isBlog={false}
               title="Rencia Rent App"
               description="A rent app that I am designing to revolutionize the real estate industry in Ghana. The app will allow users to rent properties, pay rent, and request maintenance services. The app will also allow property owners to list their properties, receive rent payments, and manage their properties. There's no landlord interaction whatsover."
               ghLink="https://github.com/Pazil24/rencia-frontend"
-              demoLink="https://blogs.soumya-jit.tech/"
+              demoLink=""
             />
           </Col>
-
-          {/* <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={editor}
-              isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
-            />
-          </Col> */}
-
-          {/* <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={leaf}
-              isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
-            />
-          </Col> */}
         </Row>
       </Container>
     </Container>
