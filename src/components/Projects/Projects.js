556e8e2f4fc3c94d16eb45b6493f975aaa1c2cf5
Pazil24 <azilafupriscilla@gmail.com>
2024-05-15 04:21:37 +0000

    Added demo link for Rencia

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -22,7 +22,7 @@ function Projects() {
               imgPath={sarah}
               isBlog={false}
               title="Sarah's Portfolio"
-              description="This is a group portfolio project of a fictional photographer called Sarah. She is a potrait photographer that specializes in headshots for actors and models. She is based in London but provides mobile services and also available to travel. She currently needs a portfolio website to enable her showcase her work so as to reach more clients."
+              description="This is a group portfolio project of a fictional photographer called Sarah. She is a portrait photographer that specializes in headshots for actors and models. She is based in London but provides mobile services and also available to travel. She currently needs a portfolio website to enable her showcase her work so as to reach more clients."
               ghLink="https://github.com/MutiatBash/Sarahs-Portfolio/tree/main"
               demoLink="https://sarahs-portfolio-project.vercel.app/index.html"
             />
@@ -35,7 +35,7 @@ function Projects() {
               title="Rencia Rent App"
               description="A rent app that I am designing to revolutionize the real estate industry in Ghana. The app will allow users to rent properties, pay rent, and request maintenance services. The app will also allow property owners to list their properties, receive rent payments, and manage their properties. There's no landlord interaction whatsover."
               ghLink="https://github.com/Pazil24/rencia-frontend"
-              demoLink=""
+              demoLink="https://youtu.be/YaI90XVbWPA"
             />
           </Col>
         </Row>
